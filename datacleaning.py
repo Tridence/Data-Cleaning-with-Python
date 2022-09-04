2b4eaf3217a5b6cc546e880313c0f2de3ab1f66f
tridence <brianwanjalabarasa@gmail.com>
2022-09-04 12:00:30 +0300

    Peeping our Columns

---
+++ datacleaning.py
@@ -0,0 +1,8 @@
+#Import Libraries
+import pandas as  pd 
+import numpy as np 
+import matplotlib.pyplot as plt
+import seaborn as sns 
+
+#Read CSV data: Property
+df = pd.read_csv("property data.csv")
